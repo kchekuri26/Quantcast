bc30ba8187e251e9629d4ceac0415ae38761dae6
kchekuri26 <57776360+kchekuri26@users.noreply.github.com>
2022-02-19 18:51:06 -0800

    All testing done.

---
+++ src/test/java/SystemTests.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+
+public class SystemTests {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    @DisplayName("Check for single output")
+    void testSingleOutput() {
+        String[] args = {"test.csv", "-d", "2018-12-09"};
+        Driver.main(args);
+        Assertions.assertEquals("AtY0laUfhglK3lC7", outContent.toString().trim());
+    }
+
+    @Test
+    @DisplayName("Check for multiple outputs")
+    void testMultipleOutputs() {
+        String[] args = {"test.csv", "-d", "2018-12-08"};
+        Driver.main(args);
+        String[] expected = {"SAZuXPGUrfbcn5UA", "4sMM2LxV07bPJzwf", "fbcn5UAVanZf6UtG"};
+        String[] actual = outContent.toString().trim().split("\\R");
+        Arrays.sort(expected);
+        Arrays.sort(actual);
+        Assertions.assertTrue(expected.length == actual.length && Arrays.equals(expected, actual));
+    }
+
+    @Test
+    @DisplayName("Check for no outputs")
+    void testZeroOutputs() {
+        String[] args = {"test.csv", "-d", "2018-12-06"};
+        Driver.main(args);
+        Assertions.assertEquals("", outContent.toString().trim());
+    }
+
+}
